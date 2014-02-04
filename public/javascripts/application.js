2c462a0ebea21bc911bb8ab4c2d8a5706d3bdfba
Ronan McHugh <mchugh.r@gmail.com>
2014-02-04 14:37:59 +0100

    styling and nav functionality.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+    $('ul.nav a').click( function() {
+            id = $(this).attr('id').substring(3);
+            if (id.length > 1) {
+                $('#' + id).toggleClass('hidden');
+            }
+        }
+    )
+    $('#user-data button').click(function(){
+        $('#user-data').toggleClass('hidden');
+    })
+})  ;
